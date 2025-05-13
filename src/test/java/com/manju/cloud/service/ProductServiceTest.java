c91d4e34ca7d7b0b568ddcea0856aec90e5dd70c
mmanju032 <mmanju032@gmail.com>
2025-05-13 22:56:56 +0530

    added some more junit5 feature for test

--- src/test/java/com/manju/cloud/service/ProductServiceTest.java
+++ src/test/java/com/manju/cloud/service/ProductServiceTest.java
@@ -6,12 +6,16 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -31,6 +35,18 @@ public class ProductServiceTest {
 	private ProductRepository repo;
 	
 	private Product productEntity;
+	
+
+	@DisplayName("justCheckingBeforeAll")
+	@BeforeAll
+	public static void beforeAllTest() {
+		
+		System.out.println("this is a dummy test for BeforeAll method");
+		// This method invoked only once at the beginning before other tests starts
+		// make sure this is static method otherwise it will fail
+		// else we need to use @TestInstance(LifeCycle.PER_CLASS) To use it at instance level 
+				
+	}
 	
 	@BeforeEach
 	public void setup(TestInfo testInfo) {
@@ -80,6 +96,17 @@ public class ProductServiceTest {
 	public void ingoreTest() {
 		
 		System.out.println("this is a dummy test for demo");		
+				
+	}
+	
+	@DisplayName("justCheckingAfterAll")
+	@AfterAll
+	public static void afterAllTest() {
+		
+		System.out.println("this is a dummy test for AfterAll method");
+		// This method invoked only once at the end after all other tests are executed
+		// make sure this is static method otherwise it will fail
+		// else we need to use @TestInstance(LifeCycle.PER_CLASS) To use it at instance level 
 				
 	}
 
